534993c18c4bf60a63e5a97527d4882450ed7c08
Min Jae David Park <minjaedavidpark@Mins-MacBook-Air.local>
2023-11-14 16:40:48 -0500

    applications

--- p2/petpal/applications/views.py
+++ p2/petpal/applications/views.py
@@ -19,7 +19,7 @@ class AppViewSet(viewsets.ModelViewSet):
         if status_filter:
             queryset = queryset.filter(status=status_filter)
 
-        if self.request.user.is_authenticated and hasattr(self.request.user, 'petshelter'):
+        if self.request.user.is_authenticated and hasattr(self.request.user, 'shelter_name'):
             shelter = self.request.user.petshelter
             queryset = queryset.filter(shelter=shelter)
 
@@ -28,29 +28,21 @@ class AppViewSet(viewsets.ModelViewSet):
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
 
-        # Check if the status is the only field being updated
-        if set(request.data.keys()) - {'status'}:
-            return Response(
-                {'error': 'Details of an application cannot be updated once submitted/created, except for its status.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        shelter = getattr(self.request.user, 'petshelter', None)
-        pet_seeker = getattr(self.request.user, 'petseeker', None)
+        shelter = hasattr(self.request.user, 'shelter_name')
 
         # Shelter can only update the status from pending to accepted or denied
-        if shelter and instance.status == Application.PENDING and request.data['status'] in [Application.ACCEPTED, Application.DENIED]:
-            serializer = self.get_serializer(instance, data=request.data, partial=True)
-            serializer.is_valid(raise_exception=True)
-            self.perform_update(serializer)
-            return Response(serializer.data)
+        if shelter and instance.status == 'pending' and request.data['status'] in ['accepted', 'rejected']:
+            instance.status = request.data['status']
+            instance.save()
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         # Pet seeker can only update the status from pending or accepted to withdrawn
-        elif pet_seeker and instance.status in [Application.PENDING, Application.ACCEPTED] and request.data['status'] == Application.WITHDRAWN:
-            serializer = self.get_serializer(instance, data=request.data, partial=True)
-            serializer.is_valid(raise_exception=True)
-            self.perform_update(serializer)
-            return Response(serializer.data)
+        elif not shelter and instance.status in ['pending', 'accepted'] and request.data['status'] == 'withdrawn':
+            instance.status = request.data['status']
+            instance.save()
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(
             {'error': 'Invalid status update for the current user and application status.'},
