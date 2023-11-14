8d5286423563066165839e0fc76c3696785d1ab3
Min Jae David Park <minjaedavidpark@Mins-MacBook-Air.local>
2023-11-14 15:25:15 -0500

    added pagination settings

--- p2/petpal/petpal/settings.py
+++ p2/petpal/petpal/settings.py
@@ -77,10 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "petpal.wsgi.application"
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10, 
-}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -150,6 +146,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, 
 }
 
 # JWT token lifetime
