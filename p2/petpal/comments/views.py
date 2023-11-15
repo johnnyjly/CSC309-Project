d62b6403032a7647265302c052b497f70a5d41eb
Gavin Pu <gavinpu27@gmail.com>
2023-11-14 23:23:00 -0500

    New serializers and allows updating of application update_time

--- p2/petpal/comments/views.py
+++ p2/petpal/comments/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseNotFound
+from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView
+from datetime import datetime
 from .models import *
 from .serializers import *
 
@@ -10,30 +11,46 @@ from .serializers import *
 
 
 class CommentOnShelterListCreate(ListCreateAPIView, LoginRequiredMixin):
-    serializer_class = CommentOnShelterSerializer
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return CommentOnShelterListSerializer
+        elif self.request.method == 'POST':
+            return CommentOnShelterCreateSerializer
 
     def get_queryset(self):
-        pet_shelter = get_object_or_404(PetShelter, ID=self.kwargs['pk'])
+        pet_shelter = get_object_or_404(PetShelter, username=self.kwargs['username'])
         return CommentOnShelter.objects.filter(pet_shelter=pet_shelter).order_by('creation_time')
 
     def perform_create(self, serializer):
-        pet_shelter = get_object_or_404(PetShelter, ID=self.kwargs['pk'])
-        comment = CommentOnShelter.objects.create(**serializer.validated_data, pet_shelter=pet_shelter)
+        pet_shelter = get_object_or_404(PetShelter, username=self.kwargs['username'])
+        comment = CommentOnShelter.objects.create(**serializer.validated_data,
+                                                  commenter=self.request.user,
+                                                  creation_time=datetime.now(),
+                                                  pet_shelter=pet_shelter,)
         comment.save()
 
 
 class CommentOnApplicationListCreate(ListCreateAPIView, LoginRequiredMixin):
-    serializer_class = CommentOnApplicationSerializer
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return CommentOnApplicationListSerializer
+        elif self.request.method == 'POST':
+            return CommentOnApplicationCreateSerializer
 
     def get_queryset(self):
         application = get_object_or_404(Application, ID=self.kwargs['pk'])
-        if self.request.user not in [application.applicant, application.shelter]:
-            return HttpResponseNotFound()
+        if self.request.user.username not in [application.applicant.username, application.shelter.username]:
+            raise Http404
         return CommentOnApplication.objects.filter(application=application).order_by('creation_time')
 
     def perform_create(self, serializer):
         application = get_object_or_404(Application, ID=self.kwargs['pk'])
-        if self.request.user not in [application.applicant, application.shelter]:
-            return HttpResponseNotFound()
-        comment = CommentOnApplication.objects.create(**serializer.validated_data, application=application)
+        if self.request.user.username not in [application.applicant.username, application.shelter.username]:
+            raise Http404
+        comment = CommentOnApplication.objects.create(**serializer.validated_data,
+                                                      commenter=self.request.user,
+                                                      creation_time=datetime.now(),
+                                                      application=application,)
+        application.update_time = datetime.now()
         comment.save()
+        application.save()
