58863695ba2fc8f13d77c35e01c71051f13d5b0e
Gavin Pu <gavinpu27@gmail.com>
2023-11-14 23:21:13 -0500

    Use pet shelter username for shelter comments views

--- p2/petpal/comments/urls.py
+++ p2/petpal/comments/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'comments'
 
 urlpatterns = [
-    path('pet_shelters/<int:pk>/', views.CommentOnShelterListCreate.as_view()),
+    path('pet_shelters/<str:username>/', views.CommentOnShelterListCreate.as_view()),
     path('applications/<int:pk>/', views.CommentOnApplicationListCreate.as_view()),
 ]
