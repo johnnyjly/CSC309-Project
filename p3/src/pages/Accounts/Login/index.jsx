c0679f91bf381255304c59b2724254e8fe439a61
Lingyun Jiang <48067740+johnnyjly@users.noreply.github.com>
2023-12-06 17:51:07 -0500

    Update index.jsx
    removed the unecessary import of bootstrap in login page

--- p3/src/pages/Accounts/Login/index.jsx
+++ p3/src/pages/Accounts/Login/index.jsx
@@ -1,8 +1,3 @@
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
-
 // import header and footer (better to use layout but just stick with this for now)
 import Header from '../../../components/Header/Header.jsx';
 import Footer from '../../../components/Footer/Footer.jsx';
