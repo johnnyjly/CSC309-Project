91d35822c80d9c23038cf02c3172eaf8a74303d5
Lingyun Jiang <rac.angel.jly@gmail.com>
2023-12-06 14:24:30 -0500

    Add corsheaders to the backend

--- p2/petpal/petpal/settings.py
+++ p2/petpal/petpal/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "accounts",
     "applications",
     "blogs",
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -161,3 +163,8 @@ SIMPLE_JWT = {
 
 # custom user model
 AUTH_USER_MODEL = "accounts.MyUser"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
