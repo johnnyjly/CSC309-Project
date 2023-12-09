0a060e43d7b6adecf1dd88b5055cbac22e0e15a8
Ryukinou <mahirhassan16@gmail.com>
2023-12-09 12:13:18 -0500

    fixed home

--- p3/src/pages/Home.js
+++ p3/src/pages/Home.js
@@ -1,29 +1,26 @@
 // Home.js
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import Header from '../components/Header/Header.jsx';
-import Footer from '../components/Footer/Footer.jsx';
+import '../styles/home.css'
 
 const Home = () => {
-  const navigate = useNavigate();
-  const authToken = localStorage.access;
-  
-  useEffect(() => {
-    if (authToken != null) {
-      navigate('/listings/');
-    }
-    else {
-      navigate('/login/');
-    }
-  })
-
   return (
     <div>
-        <Header />
-        <Footer />
+      <Header />
+      <div id="about" class="about">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6 offset-md-6">
+                  <div class="titlepage text_align_right">
+                     <h1>Find your companion.</h1>
+                     <p>Embracing the transformative power of adoption, our platform serves as a bridge, connecting these precious souls with individuals and families ready to embark on a journey filled with tail wags, purrs, and countless shared memories. Join us in creating heartwarming tales of adoption, where every pet's story is a testament to the enduring bond between humans and their faithful companions. Adopt, love, and let the magic of companionship unfold.</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
     </div>
   );
 };
-
 export default Home;
